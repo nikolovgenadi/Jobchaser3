e110f1db774a16dd816f841ef266a580c1c60214
Genadi Nikolov <genadi123.nik@gmail.com>
2024-09-02 21:06:02 +0200

    compiled

--- dist/components/navbar.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Navbar, Container, Nav, Form, FormControl } from "react-bootstrap";
-function NavbarComponent({ searchQuery, setSearchQuery }) {
-    const handleSearchChange = (event) => {
-        setSearchQuery(event.target.value);
-    };
-    return (_jsx(Navbar, { bg: "light", expand: "lg", children: _jsxs(Container, { children: [_jsx(Navbar.Brand, { href: "#home", children: "Navbar" }), _jsx(Navbar.Toggle, { "aria-controls": "basic-navbar-nav" }), _jsxs(Navbar.Collapse, { id: "basic-navbar-nav", children: [_jsxs(Nav, { className: "me-auto", children: [_jsx(Nav.Link, { href: "#home", children: "Jobs" }), _jsx(Nav.Link, { href: "#home", children: "Signup" }), _jsx(Nav.Link, { href: "#home", children: "Sign-in" })] }), _jsx(Form, { className: "d-flex", children: _jsx(FormControl, { type: "search", value: searchQuery, onChange: handleSearchChange, placeholder: "Search", className: "me-2", "aria-label": "Search" }) })] })] }) }));
-}
-export default NavbarComponent;
